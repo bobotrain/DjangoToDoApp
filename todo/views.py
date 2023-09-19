2a7f1fe7c115ba87421820cf0bc3a7a2937ec4b2
bobotrain <chlrudqh97@naver.com>
2023-09-19 17:16:04 +0900

    전체조회화면 수정

--- todo/views.py
+++ todo/views.py
@@ -4,4 +4,4 @@ from .models import Todo
 # Create your views here.
 def todo_list(request):
     todos = Todo.objects.filter(complete=False)
-    return render(request, 'todo.todo_list.html', {'todos': todos})
+    return render(request, 'todo/todo_list.html', {'todos': todos})
