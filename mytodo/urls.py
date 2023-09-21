de33e43451056c9ec9d5f1524cf34c49b676de5e
bobotrain <chlrudqh97@naver.com>
2023-09-21 14:34:34 +0900

    생성기능 v_2

--- mytodo/urls.py
+++ mytodo/urls.py
@@ -3,6 +3,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('todo/', include('todo.urls')),
+    #path('todo/', include('todo.urls')),
     path('', include('todo.urls')),
 ]
